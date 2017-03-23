53d47ac14c267548dc23d702192a0acb4cfac793
bhoomi0104 <bhoomi.vaghasiya12@gmail.com>
2017-03-23 22:58:20 +0530

    inital commit

---
+++ ProductApp/ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] p = new Product[]
+        {
+            new Product {id=1,name="x" }
+        };
+        public IEnumerable<Product> GetAllProduct()
+        {
+            return p;
+        }
+    }
+}
